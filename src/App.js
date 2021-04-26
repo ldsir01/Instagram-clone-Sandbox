a0932b351d89724da0ec21adf97749c1d10d8a90
ldsir01 <59383433+ldsir01@users.noreply.github.com>
2021-04-26 14:43:34 +0530

    Added Footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./styles.css";
 import Header from "./MyComponents/Header";
+import Footer from "./MyComponents/Footer";
 export default function App() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
